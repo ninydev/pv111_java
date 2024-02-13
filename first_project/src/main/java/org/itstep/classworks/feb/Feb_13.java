348d8f2b6f0c9bdffbc2535134d3f8f51e9b0839
Oleksandr Nykytin <keeper@ninydev.com>
2024-02-13 19:21:49 +0200

    Stream Api

--- first_project/src/main/java/org/itstep/classworks/feb/Feb_13.java
+++ first_project/src/main/java/org/itstep/classworks/feb/Feb_13.java
@@ -10,10 +10,30 @@ public class Feb_13 implements Runnable
 
     @Override
     public void run() {
-
+        asCount();
     }
 
 
+    private void asCount() {
+
+        // Если у меня операции с количеством описаны на CRUD коллекции - и поле хранится отдельно то так лушче
+        System.out.println(people.size());
+
+        // В этом случае будет построен поток - и количество элементов будет пересчитано
+        System.out.println(people.stream().count());
+
+        // В случае - когда нужно перебрать все элементы до построения ответа - построить поток - боле удобный способ
+        System.out.println(
+                people.stream() // формирует элементы коллекции в поток
+                        .filter(p-> p.getEye()== AvailableColor.blue) // формирует новый поток (промежуточная)
+                        .filter(p-> p.getAge() > 60) // из промежуточного потока - формирует новый поток (промежуточная)
+                        .count() // в отсееных элементах считает количество - терминальная (окончательная опреация
+        );
+    }
+
+
+
+//*--------------------------------------------------------------------------------------------------------------
     private ArrayList<Person> people;
     private void buildPeopleList () {
         people = new ArrayList<>();
