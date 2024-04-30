13cb07adcb2859896487e0e72584084dd87931ba
Oleksandr Nykytin <keeper@ninydev.com>
2024-04-30 19:31:40 +0300

    Android Files

---
+++ Android/MyLocalStorage/app/src/main/java/com/itstep/mylocalstorage/MainActivity.java
@@ -0,0 +1,82 @@
+package com.itstep.mylocalstorage;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+
+public class MainActivity extends AppCompatActivity {
+
+    String FILE_NAME = "myText.txt";
+
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_main);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+
+        inputText = findViewById(R.id.inputText);
+        outputText = findViewById(R.id.outputText);
+
+        readText();
+
+        ((Button) findViewById(R.id.btnUpdate)).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                saveText();
+                readText();
+            }
+        });
+
+    }
+
+
+    EditText inputText;
+    TextView outputText;
+
+    protected void saveText() {
+        try (FileOutputStream fos = openFileOutput(FILE_NAME, MODE_PRIVATE)) {
+            String txt = String.valueOf(inputText.getText());
+
+            fos.write(txt.getBytes());
+            toast("File Save");
+        }catch (Exception e) {
+            toast(e.getMessage());
+        }
+    }
+
+
+    protected void readText() {
+        try (FileInputStream fis = openFileInput(FILE_NAME)){
+            byte[] buffer = new byte[fis.available()];
+            fis.read(buffer);
+            String txt = new String(buffer);
+            outputText.setText(txt);
+        }catch (Exception e) {
+            toast(e.getMessage());
+        }
+    }
+
+
+    protected void toast(String message) {
+        Toast.makeText(this, message, Toast.LENGTH_LONG).show();
+    }
+}
